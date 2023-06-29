32b1537758435a1803352ba3f032c74de8cf4370
aadams36 <138162105+aadams36@users.noreply.github.com>
2023-06-29 16:07:05 -0400

    Add files via upload

---
+++ Destination.java
@@ -0,0 +1,20 @@
+package com.example.mylovelyfxapp;
+
+public class Destination {
+    public Destination(String jekyllIsland) {
+    }
+
+    public String getDescription() {
+        return null;
+    }
+
+    public String[] getPictures() {
+        return null;
+    }
+
+    public void setDescription(String s) {
+    }
+
+    public void addPicture(String image) {
+    }
+}
